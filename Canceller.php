c6e3d4dcdffdcdc31c41aca7e42b42a614509b8a
09429992328 <lolitacanceller45@gmail.com>
2021-04-12 11:31:51 +0800

    new message

---
+++ Canceller.php
@@ -0,0 +1,31 @@
+!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+	<?php 
+include("Animal.php");
+
+	?>
+
+<body>
+
+
+<?Php
+
+$Sumatran = new Tiger("Sumatran ", "Orange");
+echo "Its a ".$Sumatran ->get_breed();
+echo " <br>Its color ".$Sumatran ->get_color();
+echo "<br>It lives in Plain<br><br>";
+
+$Chihuahua = new Dog("Chihuahua", "Gray");
+$Chihuahua->set_Owner("Bieber");
+echo "its a".$Chihuahua->get_breed();
+echo " <br>Its color ".$Chihuahua->get_color();
+echo "<br>Her master is <br>".$Chihuahua->get_owner();
+
+
+?>
+
+</body>
+</html>
