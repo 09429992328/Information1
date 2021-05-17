379b737a8916d4e5ccb5293cd71ae5298d367c4d
09429992328 <78528863+09429992328@users.noreply.github.com>
2021-05-17 15:51:26 +0800

    Update navigator.php

--- MYPRO1/pages/navigator.php
+++ MYPRO1/pages/navigator.php
@@ -47,7 +47,7 @@ include("styles.php");
 	</style>
 	
 		<body>
-			<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+			<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-gray border-bottom box-shadow">
       		<h5 class="my-0 mr-md-auto font-weight-normal">MySite</h5>
       		<nav class="my-2 my-md-0 mr-md-3">
         	<a class="p-2 text-dark" href="../index.php">Home</a>
